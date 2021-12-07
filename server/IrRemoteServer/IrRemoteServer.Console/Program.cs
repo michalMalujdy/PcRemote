ebe074dcffb0a6c219dd85a42cd819dda8b3345e
Michal Malujdy <mima@scalepoint.com>
2021-12-07 18:40:11 +0100

    Refactor into projects

--- server/IrRemoteServer/IrRemoteServer.Console/Program.cs
+++ server/IrRemoteServer/IrRemoteServer.Console/Program.cs
@@ -1,60 +1,7 @@
-﻿using System.Globalization;
-using System.IO.Ports;
+﻿using IrRemoteServer.Core.Infrastructure;
+using IrRemoteServer.Core.Services;
 
-var serialPort = new SerialPort();
-serialPort.PortName = "COM6";
-serialPort.BaudRate = 9600;
-serialPort.ReadTimeout = 500;
-serialPort.WriteTimeout = 500;
+var serialCommunication = new SerialCommunicationService();
+var messageHandler = new MessageHandler();
 
-var readThread = new Thread(Read);
-
-serialPort.Open();
-readThread.Start();
-
-Console.ReadLine();
-
-readThread.Join();
-serialPort.Close();
-
-void Read()
-{
-    while (true)
-    {
-        try
-        {
-            var message = serialPort.ReadLine();
-
-            var parts = message.Split(' ');
-            if (parts.Contains("Repeat") || parts.Contains("Protocol=UNKNOWN"))
-            {
-                continue;
-            }
-
-            var valueHex = parts[2].Substring(8);
-            var value = Convert.ToInt32(valueHex , 16);
-
-            Console.WriteLine(value);
-
-            dynamic ws = Microsoft.VisualBasic.Interaction.CreateObject("WScript.Shell", "");
-            if (value == 22)
-            {
-                ws.SendKeys("\u00AD"); // mute or unmute
-            }
-            if (value == 74)
-            {
-                ws.SendKeys("\u00B3"); // start stop
-            }
-            if (value == 27)
-            {
-                ws.SendKeys("\u00AF"); // vol up
-            }
-            if (value == 26)
-            {
-                ws.SendKeys("\u00AE"); // vol down
-            }
-
-        }
-        catch (TimeoutException) { }
-    }
-}
+serialCommunication.Start(messageHandler.Handle);
---
+++ server/IrRemoteServer/IrRemoteServer.Core/Services/MessageHandler.cs
@@ -0,0 +1,40 @@
+﻿namespace IrRemoteServer.Core.Services;
+
+public class MessageHandler
+{
+    public void Handle(string message)
+    {
+        var properties = message.Split(' ');
+
+        if (properties.Contains("Repeat") || properties.Contains("Protocol=UNKNOWN"))
+        {
+            return;
+        }
+
+        var valueHex = properties[2].Substring(8);
+        var value = Convert.ToInt32(valueHex , 16);
+
+        Console.WriteLine(value);
+
+        dynamic ws = Microsoft.VisualBasic.Interaction.CreateObject("WScript.Shell", "");
+
+        switch (value)
+        {
+            case 22:
+                ws.SendKeys("\u00AD"); // mute or unmute
+                break;
+            case 74:
+                ws.SendKeys("\u00B3"); // start stop
+                break;
+            case 27:
+                ws.SendKeys("\u00AF"); // vol up
+                break;
+            case 26:
+                ws.SendKeys("\u00AE"); // vol down
+                break;
+            case 21:
+                ws.SendKeys("\u00B7"); // turn off
+                break;
+        }
+    }
+}
