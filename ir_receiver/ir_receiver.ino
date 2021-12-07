3f07a0a62fa8dbce1fe11c20380bad05f35a370d
Michal Malujdy <mima@scalepoint.com>
2021-12-07 19:31:21 +0100

    Make message Json

--- ir_receiver/ir_receiver.ino
+++ ir_receiver/ir_receiver.ino
@@ -1,17 +1,35 @@
 #include <IRremote.h>
 
-int IR_RECEIVE_PIN = 11; // Receive on pin 11
+int IR_RECEIVE_PIN = 11;
+int previousCommand = INT8_MIN;
 
 void setup()
 {
   Serial.begin(9600);
-  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK); // Start the receiver, enable feedback LED, take LED feedback pin from the internal boards definition
+  IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);
 
 }
 
-void loop() {
+void loop()
+{
   if (IrReceiver.decode()) {
-    IrReceiver.printIRResultShort(&Serial);
-    IrReceiver.resume(); // Continue receiving
+    
+    if(IrReceiver.decodedIRData.protocol != UNKNOWN && IrReceiver.decodedIRData.protocol != PULSE_DISTANCE && IrReceiver.decodedIRData.protocol != RC5)
+    {
+      Serial.println(buildJsonMessage());
+    }
+    
+    previousCommand = IrReceiver.decodedIRData.command;
+    IrReceiver.resume();
   }
+}
+
+String buildJsonMessage()
+{
+  return "{\"command\":" + String(IrReceiver.decodedIRData.command) + String(",\"isRepeat\":") + boolToString(previousCommand == IrReceiver.decodedIRData.command) + String("}");
+}
+
+String boolToString(bool value)
+{
+  return value ? "true" : "false";
 }
---
+++ server/IrRemoteServer/IrRemoteServer.Core/Models/Message.cs
@@ -0,0 +1,7 @@
+﻿namespace IrRemoteServer.Core.Models;
+
+public class Message
+{
+    public int Value { get; set; }
+    public bool IsRepeat { get; set; }
+}
